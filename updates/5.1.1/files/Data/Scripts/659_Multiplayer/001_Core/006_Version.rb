15fdb1ca140d69cc2451f1d95f53c0376eda9e56
sKarreku <petronijevicalekss@gmail.com>
2026-02-19 21:42:22 +0100

    Release v5.1.1

---
+++ updates/5.1.1/files/Data/Scripts/659_Multiplayer/001_Core/006_Version.rb
@@ -0,0 +1,10 @@
+#===============================================================================
+# Multiplayer Mod Version
+#===============================================================================
+# Used by external updater to check current version
+# This file is automatically updated by update_publisher.rb
+#===============================================================================
+
+module MultiplayerVersion
+  CURRENT_VERSION = "5.1.1"
+end
